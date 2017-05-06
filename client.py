f6b4808cbfa1b90d9dfc023684318312e5fe33f6
michaelburke1 <michaeljburke18@gmail.com>
2017-05-05 21:25:37 -0400

    getting started

--- client.py
+++ client.py
@@ -14,12 +14,12 @@ dataPort    = 9002
 ID = 0
 
 class userSpace:
-    def __init__(self, ID):
+    def __init__(self, connection):
+        self.conn = connection
         pygame.init()
         self.size = self.width, self.height = 640, 420
         self.black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
-        #self.ID = ID
 
         self.you = Player()
         self.opp = Player()
@@ -40,12 +40,20 @@ class userSpace:
     def loop(self):
         if event.type == KEYDOWN:
             self.you.move(event.key)
+            xMouse, yMouse = pygame.mouse.get_pos()
+            self.conn.senData(self.sendInfo(xMouse,yMouse))
+                    
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            xMouse, yMouse = pygame.mouse.get_pos()
+            self.fired = true
+            self.conn.sendData(self.sendInfo(xMouse, yMouse))
+            self.fired = false
 
-    def sendInfo(self):
+    def sendInfo(self, x, y):
         self.pYou = str(self.you.playerX) + "," + str(self.you.playerY)
 
-        xMouse, yMouse = pygame.mouse.get_pos()
-        self.pMouse = str(xMouse) + "," + str(yMouse)
+        #xMouse, yMouse = pygame.mouse.get_pos()
+        self.pMouse = str(x) + "," + str(y)
 
         if self.fired:
             self.pLaser = "True"
@@ -55,11 +63,39 @@ class userSpace:
         self.info = str(ID) + ";" + self.pYou + ";" + self.pMouse + ";" +self.pLaser
         return self.info
 
-    def refresh(self, data):
+    def refresh(self, eventString):
+        data = eventString.split("#")
+        playerSplit = data[0]
+        projectileSplit = data[1]
+        players = playerSplit.split[";"]
+        projectiles = projectileSplit.split[";"]
+
+        for player in players:
+            curr = player.split[":"]
+            if curr[0] == str(ID):
+                self.you.playerX = int(curr[1])
+                self.you.playerY = int(curr[2])
+            else:
+                self.opp.playerX = int(curr[1])
+                self.opp.playerY = int(curr[2])
+
+
+        for player in players:
+            curr = player.split[":"]
+            if curr[0] == str(ID):
+                self.you.playerX = int(curr[1])
+                self.you.playerY = int(curr[2])
+            else:
+                self.opp.playerX = int(curr[1])
+                self.opp.playerY = int(curr[2])
+
         self.screen.fill(self.black)
         pygame.display.flip()
 
         print("data recieved from server, updating")
+
+######################################################################################
+
 
 class commandFactory(ClientFactory):
     def __init__(self):
@@ -93,7 +129,7 @@ class commandConnection(Protocol):
 class dataConnection(Protocol):
     def __init__(self, parent):
         self.connection = parent
-        self.space = userSpace()
+        self.space = userSpace(self)
 
     def connectionMade(self):
         print("data connection established...")
@@ -101,8 +137,10 @@ class dataConnection(Protocol):
     def dataReceived(self, data):
         self.space.refresh(data)
 
-    def sendData(self):
-        self.transport.write(self.space.sendInfo)
+    def sendData(self, data):
+        self.transport.write(data)
+
+#########################################################################################
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -122,9 +160,11 @@ class Player(pygame.sprite.Sprite):
             self.playerY = self.playerY + 5
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        xMouse = x
-        yMouse = y
+    def __init__(self):
+
+        pygame.sprite.Sprite.__init__(self)
+
+
 ################################################################################
 
 reactor.listenTCP(commandPort, commandFactory())
