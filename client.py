87a016b34dc8209ded0e15a8b6b9b8502cb9b7b0
michaelburke1 <michaeljburke18@gmail.com>
2017-05-05 20:55:24 -0400

    client work

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
+from twisted.internet.task import LoopingCall
 
 import pygame
 from pygame.locals import *
@@ -10,12 +11,49 @@ from pygame.compat import geterror
 commandPort = 9001
 dataPort    = 9002
 
-class meee:
-    def __init__(self):
+ID = 0
+
+class userSpace:
+    def __init__(self, ID):
         pygame.init()
         self.size = self.width, self.height = 640, 420
         self.black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
+        #self.ID = ID
+
+        self.you = Player()
+        self.opp = Player()
+        self.laser = projectile(LASER)
+        self.rock = projectile(ROCK)
+        self.info = ""
+        self.pYou = ""
+        self.pMouse = ""
+        self.pLaser = ""
+        self.pRocks = ""
+
+        self.fired = False
+
+    def main(self):
+        gameLoop = LoopingCall(self.loop)
+        gameLoop.start(.5)
+
+    def loop(self):
+        if event.type == KEYDOWN:
+            self.you.move(event.key)
+
+    def sendInfo(self):
+        self.pYou = str(self.you.playerX) + "," + str(self.you.playerY)
+
+        xMouse, yMouse = pygame.mouse.get_pos()
+        self.pMouse = str(xMouse) + "," + str(yMouse)
+
+        if self.fired:
+            self.pLaser = "True"
+        else:
+            self.pLaser = "False"
+
+        self.info = str(ID) + ";" + self.pYou + ";" + self.pMouse + ";" +self.pLaser
+        return self.info
 
     def refresh(self, data):
         self.screen.fill(self.black)
@@ -55,14 +93,38 @@ class commandConnection(Protocol):
 class dataConnection(Protocol):
     def __init__(self, parent):
         self.connection = parent
-        self.meow = meee()
+        self.space = userSpace()
 
     def connectionMade(self):
         print("data connection established...")
 
     def dataReceived(self, data):
-        self.meow.refresh(data)
+        self.space.refresh(data)
 
+    def sendData(self):
+        self.transport.write(self.space.sendInfo)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.playerX = 0
+        self.playerY = 0
+
+    def move(self, k):
+        if k == pygame.K_LEFT:
+            self.playerX = self.playerX - 5
+        elif k == pygame.K_RIGHT:
+            self.playerX = self.playerX + 5
+        elif k == pygame.K_UP:
+            self.playerY = self.playerY - 5
+        elif k == pygame.K_DOWN:
+            self.playerY = self.playerY + 5
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        xMouse = x
+        yMouse = y
 ################################################################################
 
 reactor.listenTCP(commandPort, commandFactory())
